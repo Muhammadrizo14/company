1e227affd97aa8baa0c482684cc61e34d1f254ac
Muhammadrizo14 <mtursunboev13@gmail.com>
2021-11-20 10:07:00 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+AOS.init();
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper2 = new Swiper(".yourSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
